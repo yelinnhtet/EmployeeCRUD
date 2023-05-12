c0da8bfbcdd6c22b61c634b230c6c0df34760565
Ye Linn Htet <mrlinnhtet790@gmail.com>
2023-05-13 03:46:02 +0630

    edit route name

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace EmployeeCRUD
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Employee", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Employee", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
